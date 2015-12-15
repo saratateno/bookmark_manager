28687cd7fd2efb1bdc6c2c7c6674ada48b0ccea5
willibaur <williambautista@gmail.com>
2015-12-15 16:27:54 +0000

    Step 11

---
+++ spec/features/creating_links_spec.rb
@@ -0,0 +1,14 @@
+# As a time-pressed user
+# So that I can save a website
+# I would like to add the site's address and title to my bookmark manager
+
+feature 'Creating links' do
+  scenario 'I can add links on the bookmark manager' do
+    visit '/links'
+    click_link 'Add Bookmark'
+    fill_in 'Name', with: 'BBC'
+    fill_in 'url', with: 'http://bbc.co.uk'
+    click_button 'Add'
+    expect(page).to have_content('BBC')
+  end
+end
