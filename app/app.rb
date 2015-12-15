923c93c4443242b405da743ca69d9534580190e8
Sara Tateno <sara.tateno@gmail.com>
2015-12-15 16:40:32 +0000

    11. Adds a new link to the database

--- app/app.rb
+++ app/app.rb
@@ -3,9 +3,22 @@ require_relative 'models/link'
 
 class BookmarkManager < Sinatra::Base
 
+  enable :sessions
+
   get '/links' do
+    @message = session.delete(:message)
     @links = Link.all
     erb :'links/index'
+  end
+
+  get '/add-link' do
+    erb :'links/add-link'
+  end
+
+  post '/links' do
+    Link.create(title: params[:Title], url: params[:URL])
+    session[:message] = 'Link successfully added'
+    redirect '/links'
   end
 
   # start the server if ruby file executed directly
---
+++ spec/features/creating_links_spec.rb
@@ -0,0 +1,14 @@
+# As a User
+# So that I can come back to websites I like
+# I want to add new links to the list with their title
+
+feature 'add links' do
+  scenario 'user adds a link' do
+    visit '/links'
+    click_button 'Add link'
+    fill_in('Title', with: 'Makers Academy')
+    fill_in('URL', with: 'http://www.makersacademy.com/')
+    click_button 'Submit'
+    expect(page).to have_content 'Link successfully added'
+  end
+end
