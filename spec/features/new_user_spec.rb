94f7f302591504ec6ae0767b3b16d1084c4e0bfb
Sara Tateno <sara.tateno@gmail.com>
2015-12-17 16:45:10 +0000

    CH19: Password confirmation

--- spec/features/new_user_spec.rb
+++ spec/features/new_user_spec.rb
@@ -1,7 +1,12 @@
 feature 'new users' do
   scenario 'new user signs up' do
-    expect{sign_up}.to change(User, :count).by(1)
+    expect{ sign_up }.to change(User, :count).by(1)
     expect(page).to have_content('Welcome username@gmail.com')
     expect(User.first.email).to eq 'username@gmail.com'
+  end
+
+  scenario 'user signs up with mismatching password confirmation' do
+    visit '/users/new'
+    expect{ sign_up(password_confirmation:'pssword') }.not_to change(User, :count)
   end
 end
--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -1,6 +1,9 @@
-def sign_up
+def sign_up(email: 'username@gmail.com',
+            password: 'password',
+            password_confirmation: 'password')
   visit '/users/new'
-  fill_in(:email, with: 'username@gmail.com')
-  fill_in(:password, with: 'password')
+  fill_in(:email, with: email)
+  fill_in(:password, with: password)
+  fill_in(:password_confirmation, with: password_confirmation)
   click_button 'Create account'
 end
